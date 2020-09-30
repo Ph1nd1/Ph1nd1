080d0761570f965013e84592acbf92d9f372f5f2
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2020-09-30 23:41:52 +0200

    changed style and layout

--- function.js
+++ function.js
@@ -1,10 +1,13 @@
 var i=0,text;
-    text = "Welcome to my digital office..."
+    text = "Welcome to my digital office...";
 
     function typing() {
         if(i<text.length){
-            document.getElementById("text").innerHTML += text.charAt(i); i++;
-            setTimeout(typing,90);
+            document.getElementById("text").innerHTML += text.charAt(i); i++;   
         }
+        // if(i === text.length){
+        //     i = 0;
+        // }
+        setTimeout(typing,120);
     }
     typing();
